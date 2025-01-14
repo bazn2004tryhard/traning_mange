53cdb34d9dea15ab40557bab5a8759a26f6320ef
Anh Bang <nguyencongbang21122004@gmail.com>
2025-01-14 16:23:16 +0700

    css lại coursescourses

--- NCKH_base/trainingprogram/forms.py
+++ NCKH_base/trainingprogram/forms.py
@@ -10,6 +10,14 @@ class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
         fields = '__all__'
+        widgets = {
+            'course_id': forms.TextInput(attrs={'class': 'form-control'}),
+            'course_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'credits': forms.NumberInput(attrs={'class': 'form-control'}),
+            'theory_hours': forms.NumberInput(attrs={'class': 'form-control'}),
+            'practice_hours': forms.NumberInput(attrs={'class': 'form-control'}),
+            'prerequisites': forms.SelectMultiple(attrs={'class': 'form-control'}),
+        }
 class CourseTrainingProgramForm(forms.ModelForm):
     class Meta:
         model = CourseTrainingProgram
