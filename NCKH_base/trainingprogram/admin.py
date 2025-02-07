b889758107f904c38f216960d25b401311ed82a5
Anh Bang <nguyencongbang21122004@gmail.com>
2025-02-07 22:23:17 +0700

    fix lỗi và hòan thiện dbdb

--- NCKH_base/trainingprogram/admin.py
+++ NCKH_base/trainingprogram/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import TrainingProgram, Course, CourseTrainingProgram, Major, Faculty, OptionalGroup,Role,User,UserRole,Lecturer,Student
+from .models import TrainingProgram, Course, CourseTrainingProgram, Major, Faculty, OptionalGroup,Role,User,UserRole,Lecturer,Student,Grade,Class,ClassSchedule,Enrollment,RegistrationHistory
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('course_id', 'course_name', 'credits', 'get_prerequisites')
-
+class OptionalGroupAdmin(admin.ModelAdmin):
+    list_display = ('course_type', 'group_name','description','get_program','get_course')
 class TrainingProgramAdmin(admin.ModelAdmin):
     list_display = ('program_id', 'program_name', 'StartYear', 'get_major')  # Sử dụng phương thức get_major()
 
@@ -25,7 +26,16 @@ class LecturerAdmin(admin.ModelAdmin):
     list_display = ('LecturerID','Expertise','AcademicTitle','Fullname','Email','Phone','Gender','get_faculty')
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('StudentID','AcademicYear','Class','Fullname','Dob','Gender','Address','Email','Phone','get_major')
-
+class GradeAdmin(admin.ModelAdmin):
+    list_display = ('GradeID','ContinuosAssScore','FinalExamScore','Result','Semester','AcademyYear','get_student','get_course')
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('ClassID','Classroom','AcademyYear','Semester','LecturerID','get_course')
+class ClassScheduleAdmin(admin.ModelAdmin):
+    list_display = ('ScheduleID','Date','StartTime','EndTime','Classroom','get_class')
+class EnrollmentAdmin(admin.ModelAdmin):
+    list_display = ('EnrollmentID','EnrollDate','Status','StudentID','get_class')
+class RegistrationHistoryAdmin(admin.ModelAdmin):
+    list_display = ('HistoryID','TimeStamp','Action','StudentID','ClassID','get_enrollment')
 admin.site.register(Faculty, FacultyAdmin)
 admin.site.register(Major, MajorAdmin)
 admin.site.register(TrainingProgram, TrainingProgramAdmin)
@@ -36,3 +46,9 @@ admin.site.register(User,UserAdmin)
 admin.site.register(UserRole,UserRoleAdmin)
 admin.site.register(Lecturer,LecturerAdmin)
 admin.site.register(Student,StudentAdmin)
+admin.site.register(Grade,GradeAdmin)
+admin.site.register(Class,ClassAdmin)
+admin.site.register(ClassSchedule,ClassScheduleAdmin)
+admin.site.register(Enrollment,EnrollmentAdmin)
+admin.site.register(RegistrationHistory,RegistrationHistoryAdmin)
+admin.site.register(OptionalGroup,OptionalGroupAdmin)
--- NCKH_base/trainingprogram/models.py
+++ NCKH_base/trainingprogram/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 class Faculty(models.Model):
     faculty_id = models.CharField(max_length=255, primary_key=True)
@@ -9,7 +9,7 @@ class Faculty(models.Model):
     Address = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
-        return self.faculty_name
+        return self.faculty_id
 
 class Major(models.Model):
     major_id = models.CharField(max_length=255, primary_key=True)
@@ -17,7 +17,7 @@ class Major(models.Model):
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.major_name
+        return self.major_id
 
     @property
     def get_faculty(self):
@@ -81,6 +81,10 @@ class OptionalGroup(models.Model):
 
     def __str__(self):
         return self.group_name
+    def get_program(self):
+        return self.program.program_id
+    def get_course(self):
+        return self.course.course_id
 
 # Bảng Role
 class Role(models.Model):
@@ -111,15 +115,15 @@ class User(models.Model):
 
     @property
     def get_faculty(self):
-        return self.faculty
+        return self.faculty.faculty_id
 
     @property
     def get_student(self):
-        return self.student
+        return self.student.StudentID
     
     @property
     def get_lecturer(self):
-        return self.lecturer
+        return self.lecturer.LecturerID
 
 # Bảng UserRole (Bảng trung gian User - Role)
 class UserRole(models.Model):
@@ -145,7 +149,7 @@ class Lecturer(models.Model):
         return self.LecturerID
     @property
     def get_faculty(self):
-        return self.faculty
+        return self.faculty.faculty_id
 
 # Bảng Student
 class Student(models.Model):
@@ -167,4 +171,87 @@ class Student(models.Model):
 
     @property
     def get_major(self):
-        return self.major
+        return self.major.major_id
+
+class Grade(models.Model):
+    GradeID = models.CharField(max_length=255, primary_key=True)
+    ContinuosAssScore = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    FinalExamScore = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    Result =  models.CharField(max_length=255)
+    Semester = models.CharField(max_length=255)
+    AcademyYear = models.CharField(max_length=255)
+
+    #khoa ngoai
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)  # FK
+    course = models.ForeignKey("Course",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.GradeID
+    def get_student(self):
+        return self.student.StudentID
+    def get_course(self):
+        return self.course.course_id
+class Class(models.Model):
+    ClassID = models.CharField(max_length=255, primary_key=True)
+    Classroom =  models.CharField(max_length=255)
+    AcademyYear = models.CharField(max_length=255)
+    Semester = models.CharField(max_length=255)
+    LecturerID =  models.CharField(max_length=255)
+    # khóa ngoai
+    course = models.ForeignKey("Course",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ClassID
+    def get_course(self):
+        return self.course.course_id
+
+class ClassSchedule(models.Model):
+    ScheduleID = models.CharField(max_length=255, primary_key=True)
+    Date = models.DateField()
+    StartTime = models.TimeField()
+    EndTime = models.TimeField()
+    Classroom =  models.CharField(max_length=255)
+    #khoa ngoai
+    class_id = models.ForeignKey("Class",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ScheduleID
+    def get_class(self):
+        return self.class_id.ClassID
+
+class Enrollment(models.Model):
+    C_TYPE = (
+        ('Da Dang Ky', 'Đã đăng ký'),
+        ('Dang Cho Duyet', 'Đang chờ duyệt'),
+        ('Da Huy Dang Ky', 'Đã hủy đăng ký'),
+        ('Dang Huy Dang Ky', 'Đang hủy đăng ký'),
+    )
+    EnrollmentID = models.CharField(max_length=255, primary_key=True)
+    EnrollDate = models.DateField()
+    Status = models.CharField(max_length=20, choices=C_TYPE)
+    StudentID = models.CharField(max_length=255)
+    # khóa ngoai
+    class_id = models.ForeignKey("Class",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.EnrollmentID
+    def get_class(self):
+        return self.class_id.ClassID
+class RegistrationHistory(models.Model):
+   C_TYPE =(
+       ('Dang Ky', 'Đăng ký'),
+       ('Huy Dang Ky', 'Hủy đăng ký'),
+   )
+   HistoryID = models.CharField(max_length=255,primary_key=True)
+   TimeStamp = models.TimeField()
+   Action = models.CharField(max_length=20, choices=C_TYPE)
+   StudentID = models.CharField(max_length=255)
+   ClassID = models.CharField(max_length=255)
+
+   #khoa ngoai
+   enrollment = models.ForeignKey("Enrollment",on_delete=models.CASCADE)
+   def __str__(self):
+       return self.HistoryID
+   def get_enrollment(self):
+       return self.enrollment.EnrollmentID
+    
